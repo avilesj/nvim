8bda3a1ce8bed377ddb50fe0f27ed2d6616c7ff4
Jose Aviles <jose.aviles@curebase.com>
2024-07-22 17:05:04 -0400

    add fennel

---
+++ fnl/plugins/fennel.fnl
@@ -0,0 +1,5 @@
+[{1 :nvim-treesitter/nvim-treesitter :opts {:ensure_installed [:fennel]}}
+ {1 :neovim/nvim-lspconfig
+  :opts {:servers {:fennel_ls {:filetypes [:fennel]}}}}]
+
+
---
+++ lua/plugins/fennel.lua
@@ -0,0 +1,2 @@
+-- [nfnl] Compiled from fnl/plugins/fennel.fnl by https://github.com/Olical/nfnl, do not edit.
+return {{"nvim-treesitter/nvim-treesitter", opts = {ensure_installed = {"fennel"}}}, {"neovim/nvim-lspconfig", opts = {servers = {fennel_ls = {filetypes = {"fennel"}}}}}}
