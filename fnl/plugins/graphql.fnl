efd15c0d00141af95281eddaf4490bfa05380db8
Jose Aviles <javiles@mailbox.org>
2024-07-30 22:30:50 -0400

    fix graphql

--- fnl/plugins/graphql.fnl
+++ fnl/plugins/graphql.fnl
@@ -1,10 +1,4 @@
-[[:jparise/vim-graphql]
- {1 :williamboman/mason.nvim
-  :opts (fn [_ opts]
-          (vim.list_extend opts.ensure_installed
-                           [:graphql-language-service-cli]))}
- {1 :nvim-treesitter/nvim-treesitter :opts {:ensure_installed [:graphql]}}
+[
  {1 :neovim/nvim-lspconfig
   :dependencies {}
-  :opts {:servers {:graphql {:filetypes [:graphql :typescript :typescriptreact]}}}}]
-
+  :opts {:servers {:graphql {:filetypes [:graphql :typescript :typescriptreact]}}}}]	
--- lua/plugins/graphql.lua
+++ lua/plugins/graphql.lua
@@ -1,5 +1,2 @@
 -- [nfnl] Compiled from fnl/plugins/graphql.fnl by https://github.com/Olical/nfnl, do not edit.
-local function _1_(_, opts)
-  return vim.list_extend(opts.ensure_installed, {"graphql-language-service-cli"})
-end
-return {{"jparise/vim-graphql"}, {"williamboman/mason.nvim", opts = _1_}, {"nvim-treesitter/nvim-treesitter", opts = {ensure_installed = {"graphql"}}}, {"neovim/nvim-lspconfig", dependencies = {}, opts = {servers = {graphql = {filetypes = {"graphql", "typescript", "typescriptreact"}}}}}}
+return {{"neovim/nvim-lspconfig", dependencies = {}, opts = {servers = {graphql = {filetypes = {"graphql", "typescript", "typescriptreact"}}}}}}
