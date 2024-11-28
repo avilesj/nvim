4e97f6499540dafc12603eb4d7d18c304ee96c88
Jose Aviles <javiles@mailbox.org>
2024-11-28 08:37:06 -0500

    update scheme

--- fnl/plugins/colorscheme.fnl
+++ fnl/plugins/colorscheme.fnl
@@ -1,4 +1,4 @@
 [[:rebelot/kanagawa.nvim]
  {1 :folke/tokyonight.nvim :lazy false :opts {} :priority 1000}
- {1 :LazyVim/LazyVim :opts {:colorscheme :kanagawa-dragon}}]
+ {1 :LazyVim/LazyVim :opts {:colorscheme :kanagawa}}]
 
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,2 +1,2 @@
 -- [nfnl] Compiled from fnl/plugins/colorscheme.fnl by https://github.com/Olical/nfnl, do not edit.
-return {{"rebelot/kanagawa.nvim"}, {"folke/tokyonight.nvim", opts = {}, priority = 1000, lazy = false}, {"LazyVim/LazyVim", opts = {colorscheme = "kanagawa-dragon"}}}
+return {{"rebelot/kanagawa.nvim"}, {"folke/tokyonight.nvim", opts = {}, priority = 1000, lazy = false}, {"LazyVim/LazyVim", opts = {colorscheme = "kanagawa"}}}
