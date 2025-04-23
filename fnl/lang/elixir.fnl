d125f227ff32810eb28f24062545cdfcba6efb4e
Jose Aviles <javiles@mailbox.org>
2025-04-23 09:24:10 -0400

    add elixir-ls install

--- fnl/lang/elixir.fnl
+++ fnl/lang/elixir.fnl
@@ -3,5 +3,8 @@
                  :filetypes ["elixir"]
                  :root_markers ["mix.exs"]})
 
+(if (not ((. (require :mason-registry) :is_installed) :elixir-ls))
+    (vim.cmd (.. :MasonInstall " " :elixir-ls)))
+
 (vim.lsp.enable :elixir-ls)
 ((. (require :nvim-treesitter.install) :ensure_installed) [:heex :eex])
--- lua/lang/elixir.lua
+++ lua/lang/elixir.lua
@@ -1,4 +1,8 @@
 -- [nfnl] Compiled from fnl/lang/elixir.fnl by https://github.com/Olical/nfnl, do not edit.
 vim.lsp.config("elixir-ls", {cmd = {"elixir-ls"}, filetypes = {"elixir"}, root_markers = {"mix.exs"}})
+if not require("mason-registry").is_installed("elixir-ls") then
+  vim.cmd(("MasonInstall" .. " " .. "elixir-ls"))
+else
+end
 vim.lsp.enable("elixir-ls")
 return require("nvim-treesitter.install").ensure_installed({"heex", "eex"})
